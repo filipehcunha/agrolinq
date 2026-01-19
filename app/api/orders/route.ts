b4f4458b4b6adaccc8b66498bdcad31b85327da0
GuilhermePilz <167128333+GuilhermePilz@users.noreply.github.com>
2026-01-19 17:19:15 -0300

    Create route.ts

---
+++ app/api/orders/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import Order from "@/models/Order";
+import dbConnect from "@/lib/dbConnect";
+
+export async function POST(req: Request) {
+  await dbConnect();
+  const body = await req.json();
+
+  const order = await Order.create({
+    consumidorId: body.consumidorId,
+    produtorId: body.produtorId,
+    itens: body.itens,
+    total: body.total,
+    status: "novo",
+  });
+
+  return NextResponse.json(order, { status: 201 });
+}
