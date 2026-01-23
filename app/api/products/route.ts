5efed10e7d2c06c4e652caa94528c528036a831e
GuilhermePilz <167128333+GuilhermePilz@users.noreply.github.com>
2026-01-23 18:40:03 -0300

    Update route.ts

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,4 +1,4 @@
-
+export const dynamic = 'force-dynamic';
 import { NextResponse } from 'next/server';
 import dbConnect from '@/lib/mongodb';
 import Produto from '@/models/Produto';
@@ -37,4 +37,5 @@ export async function POST(request: Request) {
         return NextResponse.json({ error: 'Erro ao criar produto.' }, { status: 500 });
     }
 }
+
 
